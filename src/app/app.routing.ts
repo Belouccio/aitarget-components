23e15500634b07614462da69dc52837d60280595
Георгий <ir-41@mail.ru>
2017-05-29 15:01:46 +0300

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard.component';
 import { NotFound404Component } from './not-found404.component';
 import { DemoFilteringComponent } from './demo-filtering/demo-filtering.component';
+import { DemoTestComponent } from './demo-test/demo-test.component';
 
 export const routes: Routes = [
   {path: '', component: DashboardComponent, pathMatch: 'full'},
@@ -10,5 +11,6 @@ export const routes: Routes = [
   {path: 'geo', loadChildren: './demo-geo/index#DemoGeoModule'},
   {path: 'detailed-targeting', loadChildren: './demo-detailed-targeting/index#DemoDetailedTargetingModule'},
   {path: 'targeting', loadChildren: './demo-targeting/index#DemoTargetingModule'},
+  {path: 'test', component: DemoTestComponent},
   {path: '**', component: NotFound404Component}
 ];
